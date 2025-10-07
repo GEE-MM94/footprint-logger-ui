316f5d733f5677b6899e340082e3ce690e22623c
GEE-MM94 <mfundomkhasibe@gmail.com>
2025-10-07 10:19:31 +0200

    Updated code

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "home.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "home.html"));
 });
 
 const PORT = process.env.PORT || 5000;
